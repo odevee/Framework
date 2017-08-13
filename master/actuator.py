d4726d19f58c84b1d4b935b46847d558b97b39cf
ThaddaeusWiedemer <thaddaeus.wiedemer@gmail.com>
2017-08-13 16:30:08 +0200

    actuator, sensor, behaviour and agent class

--- master/actuator.py
+++ master/actuator.py
@@ -1,3 +1,3 @@
-class Behaviour:
+class Actuator:
     def __init__(self):
         pass
--- master/agent.py
+++ master/agent.py
@@ -1,1 +1,11 @@
+from .entity import Entity
+from .actuator import Actuator
+from .sensor import Sensor
+from .behaviour import Behaviour
+
 class agent:
+    def __init__(self, x, y):
+        Entity.__init__(self, x, y)
+        self.actuator = Actuator()
+        self.sensor = Sensor()
+        self.behaviour = Behaviour()
