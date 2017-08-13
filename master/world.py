2ab5ab0da8ddb6d761671ba5988fcedf8fc3ecff
Christof Jugel <christof@jugel.biz>
2017-08-13 16:46:11 +0200

    world class erstellt

--- master/world.py
+++ master/world.py
@@ -1,10 +1,9 @@
-class world:
+class World:
     def __init__(self, w, h):
         self.width = w
         self.height = h
 
     # Array mit Entities
-        entities = [[0 for x in range(w)] for y in range(h)]
-    entities = [[0 for x in range(w)] for y in range(h)]
-
+        entities = [[0 for x in range (w)] for y in range(h)]
+        entities_n = entities
 
