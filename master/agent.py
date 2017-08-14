becf07e7e832f9d78604fd3ecd8a9f6922377d8a
Christof Jugel <christof@jugel.biz>
2017-08-14 10:02:47 +0200

    instruktive Kommentare

--- master/agent.py
+++ master/agent.py
@@ -1,7 +1,11 @@
 from .entity import Entity
 from .actuator import Actuator
 from .sensor import Sensor
+# Agent erbt von Entity und uebernimmt dadurch die Korrdinatenattribute. Der Konstruktor erstellt
+# Instanzen einen Actuators und eines Sensors. Die update-Funktion des Agents implementiert die Logik, die mittels Sensor
+# und Actuator umgesetzt wird.
 class Agent(Entity):
+
     def __init__(self, x, y):
         Entity.__init__(self, x, y, 0)
         self.actuator = Actuator()
--- master/food.py
+++ master/food.py
@@ -1,5 +1,5 @@
 from .poi import Poi
-
+# Food mit Koordinatien x,y aus Integer und kind, das per default 0 ist.
 class Food(Poi):
     def __init__(self, x, y, kind = 0):
         Poi.__init__(self, x, y)
