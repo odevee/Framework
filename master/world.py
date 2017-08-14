92f2e6dbb31936a508ca513d6ae8227c08e72f24
ThaddaeusWiedemer <thaddaeus.wiedemer@gmail.com>
2017-08-14 11:30:04 +0200

    minor comment differences

--- master/world.py
+++ master/world.py
@@ -5,25 +5,27 @@ from .goal import Goal
 from .entityType import EntityType
 from .entity import Entity
 import numpy as np
-#represents the world grid with all entities
+
+
+# represents the world grid with all entities
 class World:
     def __init__(self, w, h):
         self.width = w
         self.height = h
 
-        #world array which contains the entities and its copy
-        self.entities = np.empty((self.width, self.height), dtype = Entity)
+        # world array which contains the entities and its copy
+        self.entities = np.empty((self.width, self.height), dtype=Entity)
         self.entities_n = self.entities
 
-    # Spawns entities
-    def spawn(self, type, x, y):
-        if type == EntityType.Agent:
+    # spawns entities
+    def spawn(self, entityType, x, y):
+        if entityType == EntityType.Agent:
             self.entities[x][y] = Agent(x, y)
-        elif type == EntityType.Food:
+        elif entityType == EntityType.Food:
             self.entities[x][y] = Food(x, y)
-        elif type == EntityType.Goal:
+        elif entityType == EntityType.Goal:
             self.entities[x][y] = Goal(x, y)
-        elif type == EntityType.Obstacle:
+        elif entityType == EntityType.Obstacle:
             self.entities[x][y] = Obstacle(x, y)
         else:
             print("ERROR: Entity could not be spawned, type doesn't exist")
@@ -32,52 +34,52 @@ class World:
     def getSlice(self, x, y, radius):
         pass
 
-    #if two entities want to move to the same position or if an agent wants to walk into a wall,
-    #this function is called and can break the tie
+    # if two entities want to move to the same position or if an agent wants to walk into a wall,
+    # this function is called and can break the tie
     def breakTie(self, entity1, entity2, x, y):
         #entity 2 always wins
         entity1.x = x
         entity1.y = y
 
-    #moves entity through the world
+    # moves entity through the world
     def moveEntity(self, entity, x, y, xn, yn):
-        #copy entity to new position
+        # copy entity to new position
         self.entities_n[xn][yn] = entity
-        #delete entity at old position
+        # delete entity at old position
         self.entities_n[x][y] = None
 
-    #updates all entities and the world grid
+    # updates all entities and the world grid
     def update(self):
-        #call the update method of all entities in the world
+        # call the update method of all entities in the world
         for row in self.entities:
             for entity in row:
                 if (type(entity) != type(None)):
                     entity.update()
-        #copy the world grid
+        # copy the world grid
         self.entities_n = self.entities
-        #move all entities in the grid copy
+        # move all entities in the grid copy
         for x in range(self.width):
             for y in range(self.height):
-                #entity which wants to move
+                # entity which wants to move
                 entity = self.entities_n[x][y]
                 if (type(entity) != type(None)):
                     # coordinates, which the entity wants to move to
                     xn = entity.x
                     yn = entity.y
-                    #if wanted position differs from current position
+                    # if wanted position differs from current position
                     if xn != x or yn != y:
-                        #get entity at target position
+                        # get entity at target position
                         entityAtTarget = self.entities_n[xn][yn]
-                        #if there isn't one ...
+                        # if there isn't one ...
                         if type(entityAtTarget) == type(None):
-                            #move entity
+                            # move entity
                             self.moveEntity(entity, x, y, xn, yn)
                         else:
-                            #if there is food/goal ...
+                            # if there is food/goal ...
                             if entityAtTarget.walkable:
-                                #move entity
+                                # move entity
                                 self.moveEntity(entity, x, y, xn, yn)
-                            #if it's an agent/obstacle
+                            # if it's an agent/obstacle
                             else:
-                                #use the tie breaker
+                                # use the tie breaker
                                 self.breakTie(entity, entityAtTarget, x, y)
