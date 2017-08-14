ac719c7ae36668ada05d3fd0527a0980d44cf090
Christof Jugel <christof@jugel.biz>
2017-08-14 10:05:16 +0200

    instruktive Kommentare

--- master/agent.py
+++ master/agent.py
@@ -2,17 +2,15 @@ from .entity import Entity
 from .actuator import Actuator
 from .sensor import Sensor
 # Agent erbt von Entity und uebernimmt dadurch die Korrdinatenattribute. Der Konstruktor erstellt
-# Instanzen einen Actuators und eines Sensors. Die update-Funktion des Agents implementiert die Logik, die mittels Sensor
-# und Actuator umgesetzt wird.
+# Instanzen einen Actuators und eines Sensors.
 class Agent(Entity):
 
     def __init__(self, x, y):
         Entity.__init__(self, x, y, 0)
         self.actuator = Actuator()
         self.sensor = Sensor()
-
+# updatet den Agenten. Wird in jeder world-update-iteration aufgerufen
     def update(self):
-        print("Agent Update")
         tup = self.actuator.act(self.x, self.y)
         self.x = tup[0]
         self.y = tup[1]
