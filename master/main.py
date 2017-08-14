bd51c16794ab6a9fa02bdf15c3baeab2446002b7
Christof Jugel <christof@jugel.biz>
2017-08-14 02:06:26 +0200

    -

--- master/main.py
+++ master/main.py
@@ -76,11 +76,11 @@ while True:
             pygame.draw.rect(DISP_SURF, WHITE, (column*TILESIZE, row*TILESIZE, TILESIZE,TILESIZE), 1)
     #update the display
     fpsClock.tick(FPS)
-    for i in range(MAP_HEIGHT):
-        for j in range(MAP_WIDTH):
-            if isinstance(world.entities[i][j], Entity):
+    for row in world.entities:
+        for entity in row:
+            if (type(entity) != type(None)):
+                print(type(entity))
                 print("Es ist eine Entity")
-                tmp = world.entities[i][j]
-                Entity.update(world.entities[i][j])
+                entity.update()
     Render()
     pygame.display.update()
