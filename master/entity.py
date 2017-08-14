cf793f5a908de54f93b069b79d7d85d86ca3ba38
ThaddaeusWiedemer <thaddaeus.wiedemer@gmail.com>
2017-08-14 10:30:20 +0200

    minor comments

--- master/entity.py
+++ master/entity.py
@@ -1,7 +1,7 @@
 class Entity:
 
     # x und y geben die Koordinaten des Objekt Entity an (Typ Integer)
-    # wenn walkable == 1, kann man das Feld der Entität begangen werden. Wenn 0, dann nicht
+    # wenn walkable == 1, kann man das Feld der Entität betreten. Wenn 0, dann nicht
     def __init__(self, x, y, walkable = 1):
             self.x = x
             self.y = y
