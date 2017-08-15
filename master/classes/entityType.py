f091be232d4bb6160685b34580f5b0e3b55c026a
odevee <ovstarwars@gmail.com>
2017-08-15 15:06:55 +0200

    current world state can now be saved to a map file

--- master/classes/entityType.py
+++ master/classes/entityType.py
@@ -2,7 +2,7 @@ from enum import Enum
 # EnumerationType, um zwischen den unterschiedlichen Entity-Erben zu unterscheiden.
 #EntityType = Enum('EntityType', 'Agent Food Goal Obstacle')
 class EntityType(Enum):
-    Agent = 0
-    Food = 1
-    Goal = 2
-    Obstacle = 3
+    Agent      = 0
+    Food       = 1
+    Goal       = 2
+    Obstacle   = 3
