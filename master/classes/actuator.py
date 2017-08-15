f8ff6f6db971d6a9a6da9841b32c619ee7ff8a00
Sascha <rienaecker.sascha@t-online.de>
2017-08-15 23:09:51 +0200

    Eine elegantere Implementierung der zufälligen Entscheidung zwischen Nachbarfeldern mit gleichem EMpowerment.

--- master/classes/actuator.py
+++ master/classes/actuator.py
@@ -80,6 +80,13 @@ class Actuator:
     #     self.getNeighbours(slice, xa, ya, x, y - 1, range - 1, neighbours)
     #     return neighbours
 
+# calculates the empowerment of given position in the world slice
+    def calcEMP(self, slice, xa, ya, x, y):
+        neighbours = {(x,y)}
+        self.getNeighbours(slice, xa, ya, x, y, self.think_range, neighbours)
+        #print('far neighbours: ', neighbours)
+        return len(neighbours)
+
 
 # decides where to go based on empowerment
     def getAction(self, slice, x, y):
@@ -90,34 +97,23 @@ class Actuator:
         # find direct neighbours
         direct_neighbours = {(x,y)}
         self.getNeighbours(slice, x, y, x, y, self.range, direct_neighbours)
-        print('Position:', x, y)
-        print('Neighbours: ', direct_neighbours)
+        #print('Position:', x, y)
+        #print('Neighbours: ', direct_neighbours)
         # calculate empowerment
         target = (x, y)
+        #find all potential neighbours,
+        # i.e. neighbours with maximum empowerment
+        #one of them will be randomly chosen
         emp_max = -1
-        maxs = [(x,y)]
+        potentialTargets = []
         for n in direct_neighbours:
             emp = self.calcEMP(slice, x, y, n[0], n[1])
-            print('Empowerment at ', n[0], n[1], emp)
             if emp > emp_max:
+                potentialTargets = []
+            if emp >= emp_max:
                 emp_max = emp
-                target = (n[0], n[1])
-                # in case of a new max emp: refresh max set
-                maxs = [(n[0], n[1])]
-            # in case of equality: append all the cords with equal emp
-            elif emp == emp_max:
-                maxs.append((n[0], n[1]))
-        # find random max with laplace
-        n = len(maxs)
-        #print(n)
-        l = random.randint(0,n-1)
-
-        return maxs.__getitem__(l)
-
-
-# calculates the empowerment of given position in the world slice
-    def calcEMP(self, slice, xa, ya, x, y):
-        neighbours = {(x,y)}
-        self.getNeighbours(slice, xa, ya, x, y, self.think_range, neighbours)
-        #print('far neighbours: ', neighbours)
-        return len(neighbours)
+                potentialTargets.append((n[0], n[1], emp))
+        for t in potentialTargets:
+            print('Maximum empowerment at (',t[0], t[1],'): ',t[2])
+        print ('\n')
+        return random.choice(potentialTargets)
